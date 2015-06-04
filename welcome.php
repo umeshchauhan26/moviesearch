c658e2bf91812c8f248c6a81525e96cbd6a25611
umeshchauhan26 <umesh.chauhan26@gmail.com>
2015-06-04 09:29:44 -0700

    Create welcome.php
    this is the welcome page for the interface with the basic form elements

---
+++ welcome.php
@@ -0,0 +1,41 @@
+<html>
+</head>
+  </head>
+  <body>
+<form  name='movie' method='GET'>
+<fieldset>
+<legend>Search Here:</legend>
+Enter Plot:<br>
+<input type="text" name="plot" value="">
+<br>
+Enter Movie Title:<br>
+<input type="text" name="title" value="">
+<br>
+Search By Director Name:<br>
+<input type="text" name="director" value="">
+<br>
+<input type="checkbox" name="genre[]" value="Comedy"  /> Comedy 
+<input type="checkbox" name="genre[]" value="Drama"  /> Drama 
+<input type="checkbox" name="genre[]" value="Family"  /> Family 
+<input type="checkbox" name="genre[]" value="Sci-Fi"  /> Sci-Fi 
+<input type="checkbox" name="genre[]" value="Adventure"  /> Adventure 
+<input type="checkbox" name="genre[]" value="Romance"  /> Romance 
+<input type="checkbox" name="genre[]" value="Thriller"  /> Thriller 
+<input type="checkbox" name="genre[]" value="Fantasy"  /> Fantasy 
+ <input type="checkbox" name="genre[]" value="War"  /> War
+ <input type="checkbox" name="genre[]" value="Mystery"  /> Mystery
+ <input type="checkbox" name="genre[]" value="Crime"  /> Crime
+ <input type="checkbox" name="genre[]" value="Film-Noir"  /> Film-Noir
+ <input type="checkbox" name="genre[]" value="Western"  /> Western
+ <input type="checkbox" name="genre[]" value="Musical"  /> Musical
+
+<br><br>
+<button onclick="" href="#">Search</button></fieldset>
+</form>
+<fieldset >
+<legend >Search results:</legend>
+<div id="text1">
+</div>
+</fieldset>
+	</body>
+	</center>
