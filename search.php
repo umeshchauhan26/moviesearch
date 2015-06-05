91493df7b3e87419d79c9d1c07ee5b46c0c0627e
umeshchauhan26 <umesh.chauhan26@gmail.com>
2015-06-05 09:46:05 -0700

    Update search.php
    added search.php file to handle form elements of welcome file

--- search.php
+++ search.php
@@ -1,1 +1,14 @@
 <?php
+## GET FORM INPUTS
+$plot=trim($_GET["plot"]); 
+$title=trim($_GET["title"]); 
+$director=trim($_GET["director"]);
+$genre_array=$_GET["genre"];
+
+
+echo $plot;
+echo $title;
+echo $director;
+echo $genre_array;
+
+?>
